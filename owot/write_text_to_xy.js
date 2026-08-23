f27c22980e4f81301179a56741cfa5a09e2f0d2f
NotReiny^2 <142988995+nrtc2@users.noreply.github.com>
2026-08-23 13:03:32 +0700

    Create write_text_to_xy.js

---
+++ owot/write_text_to_xy.js
@@ -0,0 +1,20 @@
+async function writeTextToXY(string, color, wX, wY, wait = 0) {
+    function delay(t) {
+	    return new Promise(e => setTimeout(e, t))
+    }
+
+    let cX = wX,
+        cY = wY,
+        lines = string.toString().split("\n");
+    for (let y = 0; y < lines.length; y++) {
+        for (let x = 0; x < lines[y].length; x++) {
+            const curChr = lines[y].charAt(x);
+
+            writeCharToXY(curChr, curChr !== " " ? color : 0, cX, cY);
+
+            if (wait > 0 && isFinite(wait)) await delay(wait);
+            cX++
+        }
+        cX = wX, cY++
+    }
+}
