f91f2ec8d2d8835540b3614d69ea35fcb8fd3b57
NotReiny^2 <142988995+nrtc2@users.noreply.github.com>
2026-08-25 14:17:48 +0700

    Create minijs-console.js

---
+++ owot/minijs-console.js
@@ -0,0 +1,37 @@
+(function() {
+	const div = document.createElement("div"),
+		execButton = document.createElement("button"),
+		field = document.createElement("textarea"),
+		outputs = document.createElement("div");
+
+	execButton.textContent = "Evaluate";
+
+	div.style.position = "absolute";
+	div.style.zIndex = "9".repeat(999);
+	div.style.transform = "translateY(-50px)";
+
+	execButton.addEventListener("click", function() {
+        const out = document.createElement("pre"),
+        	inp = document.createElement("pre"),
+            val = field.value;
+        inp.textContent = val;
+		try {
+            out.textContent = eval.call(globalThis, val);
+			outputs.appendChild(inp);
+			outputs.appendChild(f)
+		} catch (e) {
+            out.textContent = e;
+			outputs.appendChild(inp);
+			outputs.appendChild(f)
+		}
+		field.value = ""
+	});
+	div.addEventListener("click", function() {
+		worldFocused = false
+	});
+
+	div.appendChild(outputs);
+	div.appendChild(field);
+	div.appendChild(execButton);
+	document.body.appendChild(div);
+})();
